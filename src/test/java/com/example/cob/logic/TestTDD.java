33fa3f8559e87fdb8788e3b155c3c3d7fc863efe
admin <rong@agile-solutions.ch>
2021-10-10 15:28:50 +0200

    initial commit

---
+++ src/test/java/com/example/cob/logic/TestTDD.java
@@ -0,0 +1,37 @@
+package com.example.cob.logic;
+
+import com.example.cob.model.Case;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TestTDD {
+
+    Case onboardingCase = new Case();
+
+    @BeforeAll
+    public void init() {
+
+
+    }
+
+    @Test
+    public void givenClientSubmitted_whenOffBoard_thenCaseStatusCancelled() {
+
+        // GIVEN
+        onboardingCase.thenApply(Strategy.rejected());
+
+        // WHEN
+        onboardingCase.thenApply(Strategy.priReview());
+
+        // THEN
+        onboardingCase.thenApply(Strategy.verify(true));s
+
+
+
+    }
+
+}
